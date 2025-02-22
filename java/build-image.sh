cfd26c4d7edcc6508f1e77a6f627147e9381bb20
Ciallo <porridgen@gmail.com>
2025-02-22 23:44:25 +0800

    fix java linkage errors in WSL2-based containers

--- java/build-image.sh
+++ java/build-image.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-docker build -t nailm/aether-java:21 \
-  --build-arg JDK_AMD64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk-21.0.6-linux-x64-b872.85.tar.gz \
-  --build-arg JDK_ARM64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk-21.0.6-linux-aarch64-b872.85.tar.gz \
+# 创建并使用 buildx 构建器
+docker buildx create --use
+
+# 构建多平台镜像并推送到 Docker Hub
+docker buildx build --platform linux/amd64,linux/arm64 -t nailm/aether-java:21 \
+  --build-arg JDK_AMD64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-21.0.6-linux-x64-b895.91.tar.gz \
+  --build-arg JDK_ARM64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-21.0.6-linux-aarch64-b895.91.tar.gz \
   .
