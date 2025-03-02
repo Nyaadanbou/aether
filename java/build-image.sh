ed93a0f5bc291854031271f834674da0498c6997
Ciallo <porridgen@gmail.com>
2025-03-03 00:22:47 +0800

    enable image push during Docker build

--- java/build-image.sh
+++ java/build-image.sh
@@ -7,4 +7,5 @@ docker buildx create --use
 docker buildx build --platform linux/amd64,linux/arm64 -t nailm/aether-java:21 \
   --build-arg JDK_AMD64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-21.0.6-linux-x64-b895.91.tar.gz \
   --build-arg JDK_ARM64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-21.0.6-linux-aarch64-b895.91.tar.gz \
+  --push
   .
