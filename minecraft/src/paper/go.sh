27ef9aa2f0638473e66070c4c497d059e694afdb
Ciallo <porridgen@gmail.com>
2025-03-05 06:11:40 +0800

    keep tmux alive after java exits

--- minecraft/src/paper/go.sh
+++ minecraft/src/paper/go.sh
@@ -6,7 +6,7 @@ SERVER=paper-unnamed
 tmux has-session -t $SERVER 2>/dev/null
 
 if [ $? != 0 ]; then
-    tmux new-session -s $SERVER -n "main" "java @jvmargs.txt -jar ignite.jar"
+    tmux new-session -s $SERVER -n "main" "java @jvmargs.txt -jar ignite.jar --nogui; bash"
 else
     tmux attach-session -t $SERVER
 fi
--- minecraft/src/velocity/go.sh
+++ minecraft/src/velocity/go.sh
@@ -6,7 +6,7 @@ SERVER=velocity-unnamed
 tmux has-session -t $SERVER 2>/dev/null
 
 if [ $? != 0 ]; then
-    tmux new-session -s $SERVER -n "main" "java @jvmargs.txt -jar velocity.jar"
+    tmux new-session -s $SERVER -n "main" "java @jvmargs.txt -jar velocity.jar; bash"
 else
     tmux attach-session -t $SERVER
 fi
