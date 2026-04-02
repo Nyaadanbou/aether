dffd0fc65ec85f215d77a9b62c15e242ba896d5c
Ciallo <porridgen@gmail.com>
2026-04-03 06:44:45 +0800

    chore: upgrade Java base image to 25

--- java/build-image.sh
+++ java/build-image.sh
@@ -8,9 +8,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 docker buildx create --use
 
 # 构建多平台镜像并推送到 Docker Hub
-docker buildx build --platform linux/amd64,linux/arm64 -t nailm/aether-java:21 \
-  --build-arg JDK_AMD64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-21.0.6-linux-x64-b895.91.tar.gz \
-  --build-arg JDK_ARM64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-21.0.6-linux-aarch64-b895.91.tar.gz \
+docker buildx build --platform linux/amd64,linux/arm64 -t nailm/aether-java:25 \
+  --build-arg JDK_AMD64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-25.0.2-linux-x64-b329.72.tar.gz \
+  --build-arg JDK_ARM64_URL=https://cache-redirector.jetbrains.com/intellij-jbr/jbrsdk_jcef-25.0.2-linux-aarch64-b329.72.tar.gz \
   --push \
   -f "${SCRIPT_DIR}/dockerfile" \
   "${SCRIPT_DIR}"
